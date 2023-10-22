0807dd759399ff535ec9e1dbc7b37c74e5ad92b5
chelinomx <107780203+chelinomx@users.noreply.github.com>
2023-10-22 16:12:00 -0500

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,1 +1,5 @@
 javascript: fetch("https://cdn.jsdelivr.net/gh/chelinomx/fsep@latest/script.js").then(r => r.text()).then(r => eval(r))
+
+
+
+javascript:fetch("//cdn.jsdelivr.net/gh/ading2210/edpuzzle-answers@latest/script.js").then(function(response) { return response.text() }).then(function(text) { eval(text) })
